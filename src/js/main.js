841cd40e51b3ae0eee9332f6d0915eaebb0b2cff
bharath8328 <84236253+bharath8328@users.noreply.github.com>
2021-08-06 23:31:25 +0530

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,29 @@
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
+const faders=document.querySelectorAll('.fade-in');
+
+const appearOptions={
+    threshold:1,
+	rootMargin: "0px 0px 300px 0px"
+};
+
+const appearOnScroll= new IntersectionObserver(
+    function(entries,appearOnScroll){
+        entries.forEach(entry=>{
+            if(!entry.isIntersecting){
+                return;
+            } else {
+                entry.target.classList.add("appear");
+                appearOnScroll.unobserve(entry.target);
+            }
+        });
+    },appearOptions);
+
+faders.forEach(fader => {
+        appearOnScroll.observe(fader);
+    }
+);
 
 (function($) {
 
